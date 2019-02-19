83bb9a5c20acf113a10158e4c4fcd09d156c37c6
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2019-02-19 15:36:14 +0900

    call RequestPermission method from android native
    it will NOT display confirm dialog...

--- PermissionSample/PermissionSample.Android/MainActivity.cs
+++ PermissionSample/PermissionSample.Android/MainActivity.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Android.App;
 using Android.OS;
 using Android.Runtime;
@@ -7,12 +9,15 @@ using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using Permission = Plugin.Permissions.Abstractions.Permission;
 
 namespace PermissionSample.Droid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        TextView _resultText;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -25,6 +30,52 @@ namespace PermissionSample.Droid
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+
+            _resultText = FindViewById<TextView>(Resource.Id.ResultText);
+
+            var phoneButton = FindViewById<Button>(Resource.Id.PhoneButton);
+            phoneButton.Click += PhoneButton_Click;
+
+            var locationButton = FindViewById<Button>(Resource.Id.LocationButton);
+            locationButton.Click += LocationButton_Click;
+
+            var cameraButton = FindViewById<Button>(Resource.Id.CameraButton);
+            cameraButton.Click += CameraButton_Click;
+        }
+
+        private async void PhoneButton_Click(object sender, EventArgs e)
+        {
+            var status = await Core.PermissionManager.RequestPermissionsAsync(Permission.Phone, Permission.Contacts);
+
+            _resultText.Text = GetPermissionStatus(status);
+        }
+
+        private async void LocationButton_Click(object sender, EventArgs e)
+        {
+            var status = await Core.PermissionManager.RequestPermissionsAsync(Permission.Location);
+
+            _resultText.Text = GetPermissionStatus(status);
+        }
+
+        private async void CameraButton_Click(object sender, EventArgs e)
+        {
+            var status = await Core.PermissionManager.RequestPermissionsAsync(Permission.Camera, Permission.MediaLibrary);
+
+            _resultText.Text = GetPermissionStatus(status);
+        }
+
+        private string GetPermissionStatus(Dictionary<Permission, PermissionStatus> status)
+        {
+            var sb = new StringBuilder();
+            sb.Append("Result:\n\n");
+
+            foreach (var pair in status)
+            {
+                sb.Append($"Permission: {pair.Key.ToString()}\n" +
+                    $"Status: {pair.Value.ToString()}\n\n");
+            }
+
+            return sb.ToString();
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
