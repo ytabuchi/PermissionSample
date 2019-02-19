f96a710e16273cf0b0341d3da4d467fc1a4783fc
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2019-02-19 10:44:03 +0900

    Add Plugin.Permission
    add plugin to related projects and update MainActivity.cs to use plugin.

--- PermissionSample/XFPermissionSample/XFPermissionSample.Android/MainActivity.cs
+++ PermissionSample/XFPermissionSample/XFPermissionSample.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
 
 namespace XFPermissionSample.Droid
 {
@@ -19,7 +20,14 @@ namespace XFPermissionSample.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
